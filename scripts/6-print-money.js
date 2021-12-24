39a92caf8228073e355120e0e7218e70d4857d87
Daniel Weinman <namniewd@gmail.com>
2021-12-23 22:50:50 -0500

    completed project

---
+++ scripts/6-print-money.js
@@ -0,0 +1,28 @@
+import { ethers } from "ethers";
+import sdk from "./1-initialize-sdk.js";
+
+// This is the address of our ERC-20 contract printed out in the step before.
+const tokenModule = sdk.getTokenModule(process.env.TOKEN_MODULE_ADDRESS);
+const ONE_BILLION = 1000 * 1000 * 1000;
+
+(async () => {
+  try {
+    // Max supply of token
+    const amount = ONE_BILLION;
+    // We use the util function from "ethers" to convert the amount
+    // to have 18 decimals (which is the standard for ERC20 tokens).
+    const amountWith18Decimals = ethers.utils.parseUnits(amount.toString(), 18);
+    // Interact with your deployed ERC-20 contract and mint the tokens!
+    await tokenModule.mint(amountWith18Decimals);
+    const totalSupply = await tokenModule.totalSupply();
+    
+    // Print out how many of our token's are out there now!
+    console.log(
+      "✅ There now is",
+      ethers.utils.formatUnits(totalSupply, 18),
+      "$GAMBLE in circulation",
+    );
+  } catch (error) {
+    console.error("Failed to print money", error);
+  }
+})();
